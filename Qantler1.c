23e788a9cbe40700c28b05cfe005679fc4db6e1e
dharunkumarV <dharunkumarv.ug22.cs@francisxavier.ac.in>
2026-02-16 15:05:44 -0500

    asked in drive

---
+++ Qantler1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {//steps to reduce to 0 asked in drive
+    int n;
+    scanf("%d",&n);
+    int count=0;
+    while(n!=0){
+        if(n%2==0){
+            n=n/2;
+        }
+        else if(n%2==1){
+            n=n-1;
+        }
+        count++;
+    }
+    printf("%d",count);
+
+    return 0;
+}
